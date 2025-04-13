514889f367a2edfd9cff90b8418ea5dfbae37967
michael0220 <asdf1984620s@gmail.com>
2025-04-13 23:39:56 +0800

    Add files via upload

---
+++ script/hero.cs
@@ -0,0 +1,26 @@
+using UnityEditor.Analytics;
+using UnityEngine;
+
+
+public class hero : MonoBehaviour
+{
+    public float speed;
+    void Start()
+    {
+        speed = 0.05f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.gameObject.transform.position += new Vector3(speed*Time.deltaTime*60, 0, 0);
+        
+    }
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if(coll.gameObject.tag == "enemy"){
+            speed = 0;
+            print("hit");
+        }
+    }
+}
