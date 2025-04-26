45bae7b73ad8afd99fc311122f9874aebe380f1f
michael0220 <asdf1984620s@gmail.com>
2025-04-27 00:18:36 +0800

    增加其他角色按鈕

--- unity/Assets/script/botton_controller.cs
+++ unity/Assets/script/botton_controller.cs
@@ -3,9 +3,17 @@ using UnityEngine;
 public class botton_controller : MonoBehaviour
 {
     public GameObject hero_prefab;
+    public GameObject hero2_prefab;
+    public GameObject enemy_prefab;
 
     public void spawn_hero()
     {
         Instantiate(hero_prefab, new Vector3(-4.0f, -1.3f, 0), Quaternion.identity);
-    }   
+    }
+    public void spawn_hero2(){
+        Instantiate(hero2_prefab, new Vector3(-4.0f, -1.3f, 0), Quaternion.identity);
+    }
+    public void spawn_enemy(){
+        Instantiate(enemy_prefab, new Vector3(5.0f, -1.3f, 0), Quaternion.identity);
+    }
 }
