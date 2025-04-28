9ca1fb0bc6a7da9b06b3a1615de0637f884f837f
michael0220 <asdf1984620s@gmail.com>
2025-04-28 13:47:23 +0800

    新增enemy攻擊

--- unity/Assets/script/enemy.cs
+++ unity/Assets/script/enemy.cs
@@ -3,13 +3,23 @@ using UnityEngine;
 public class enemy : MonoBehaviour
 {
     public int hp;
-    public int max_hp=300;
-    public float timer=0;
     public GameObject hp_bar;
+    public int max_hp=300;
+
+    bool isCollidingWithHero = false;
+    private float damageInterval = 0.8f;
+    private float damageTimer = 0f;
+
+    public float timer=0;
+    
     private float speed = 1.0f;
+
+    Collider2D enemyCollider;
+
     void Start()
     {
         hp = 300;
+        enemyCollider = GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
@@ -22,13 +32,41 @@ public class enemy : MonoBehaviour
             hp=0;
             Destroy(this.gameObject);
         }
+        if(isCollidingWithHero && damageTimer>=damageInterval){
+            DealDamageToHero();
+            damageTimer = 0f;
+        }
+        damageTimer += Time.deltaTime;
         hp_bar.transform.localScale = new Vector3((float)((float)hp/(float)max_hp), hp_bar.transform.localScale.y, hp_bar.transform.localScale.z);
     }
 
     void OnCollisionEnter2D(Collision2D other){
-        if(other.gameObject.CompareTag("hero")) speed = 0;
+        if(other.gameObject.CompareTag("hero")){
+            speed = 0f;
+            isCollidingWithHero = true;
+        }
     }
     void OnCollisionExit2D(Collision2D other){
-        if(other.gameObject.CompareTag("hero")) speed = 1.0f;
+        if(other.gameObject.CompareTag("hero")){
+            speed = 1.0f;
+            isCollidingWithHero = false;
+        }
+    }
+
+    void DealDamageToHero(){
+        if(!isCollidingWithHero) return;
+
+        ContactPoint2D[] contacts = new ContactPoint2D[10];
+        int contactsCount = enemyCollider.GetContacts(contacts);
+
+        for(int i=0;i<contactsCount;i++){
+            GameObject heroObj = contacts[i].collider != null ? contacts[i].collider.gameObject : null;
+            if(heroObj != null && heroObj.CompareTag("hero")){
+                hero heroScript = heroObj.GetComponent<hero>();
+                if(heroScript!=null){
+                    heroScript.hp -= 15;
+                }
+            }
+        }
     }
 }
--- unity/Assets/script/hero.cs
+++ unity/Assets/script/hero.cs
@@ -8,20 +8,33 @@ public class hero : MonoBehaviour
     bool isCollidingWithEnemy = false;
     float damageTimer = 0f;
     float damageInterval = 1.5f;
+
+    public int hp;
+    public int max_hp = 400;
+    public GameObject hp_bar;
+
     Collider2D heroCollider;
+
     void Start()
     {
+        hp = 400;
         heroCollider = GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(hp<=0){
+            hp=0;
+            Destroy(this.gameObject);
+        }
         if(isCollidingWithEnemy && damageTimer >= damageInterval){
             DealDamageToEnemy();
             damageTimer = 0f;
         }
         damageTimer += Time.deltaTime;
+        hp_bar.transform.localScale = new Vector3((float)((float)hp/(float)max_hp), hp_bar.transform.localScale.y, hp_bar.transform.localScale.z);
+
     }
     void OnCollisionEnter2D(Collision2D collision)
     {
@@ -43,9 +56,9 @@ public class hero : MonoBehaviour
         int contactsCount = heroCollider.GetContacts(contacts);
 
         for(int i=0;i<contactsCount;i++){
-            GameObject enemy = contacts[i].collider.gameObject;
-            if(enemy.CompareTag("enemy")){
-                enemy enemyScript = enemy.GetComponent<enemy>();
+            GameObject enemyObj = contacts[i].collider != null ? contacts[i].collider.gameObject : null;
+            if(enemyObj != null && enemyObj.CompareTag("enemy")){
+                enemy enemyScript = enemyObj.GetComponent<enemy>();
                 if(enemyScript!=null){
                     enemyScript.hp -= 20;
                 }
