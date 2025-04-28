2d04b887e9e96815a7143c1c506b3c5ae24be051
michael0220 <asdf1984620s@gmail.com>
2025-04-28 13:29:09 +0800

    優化button程式碼

--- unity/Assets/script/enemy.cs
+++ unity/Assets/script/enemy.cs
@@ -6,6 +6,7 @@ public class enemy : MonoBehaviour
     public int max_hp=300;
     public float timer=0;
     public GameObject hp_bar;
+    private float speed = 1.0f;
     void Start()
     {
         hp = 300;
@@ -14,7 +15,7 @@ public class enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        this.gameObject.transform.position -= new Vector3(1.0f*Time.deltaTime, 0, 0);
+        this.gameObject.transform.position -= new Vector3(speed*Time.deltaTime, 0, 0);
         timer += Time.deltaTime;
         if(hp<=0)
         {
@@ -22,5 +23,12 @@ public class enemy : MonoBehaviour
             Destroy(this.gameObject);
         }
         hp_bar.transform.localScale = new Vector3((float)((float)hp/(float)max_hp), hp_bar.transform.localScale.y, hp_bar.transform.localScale.z);
+    }
+
+    void OnCollisionEnter2D(Collision2D other){
+        if(other.gameObject.CompareTag("hero")) speed = 0;
+    }
+    void OnCollisionExit2D(Collision2D other){
+        if(other.gameObject.CompareTag("hero")) speed = 1.0f;
     }
 }
