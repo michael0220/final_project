bd3202d05ad65d1fdcd5305d1fe4dde43425ba3b
michael0220 <asdf1984620s@gmail.com>
2025-04-28 15:36:42 +0800

    新增enemy的spawnpoints和隨機生成

---
+++ unity/Assets/script/enemy_spawner.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class enemy_spawner : MonoBehaviour
+{
+    public Transform[] spawnpoints;
+    public GameObject enemy;
+
+    void spawn_enemy(){
+        int r = Random.Range(0, spawnpoints.Length);
+        GameObject new_enemy = Instantiate(enemy, spawnpoints[r].position, Quaternion.identity);
+    }
+
+    void Start()
+    {
+        InvokeRepeating("spawn_enemy", 2, 7);
+    }
+}
