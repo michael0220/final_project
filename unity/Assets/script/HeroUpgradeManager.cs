59887cfa84fdeb8f06bfdb3312bc866b67d99aaa
michael0220 <asdf1984620s@gmail.com>
2025-05-17 20:48:04 +0800

    修改角色升級系統

--- unity/Assets/script/HeroUpgradeManager.cs
+++ unity/Assets/script/HeroUpgradeManager.cs
@@ -8,24 +8,5 @@ public class HeroUpgradeManager : MonoBehaviour
     public int currentlevel = 1;
     public int maxlevel = 3;
 
-    private List<Hero> activeHeroes = new List<Hero>();
-
-    void Awake(){
-        if(Instance==null) Instance = this;
-        else Destroy(gameObject);
-    }
-
-    public void RegisterHero(Hero hero){
-        activeHeroes.Add(hero);
-    }
-
-    public void UpgradeAllHeroes(){
-        if(currentlevel>=maxlevel) return;
-
-        currentlevel++;
-        foreach(Hero hero in activeHeroes){
-            if(hero != null){
-            }
-        }
-    }
+    
 }
