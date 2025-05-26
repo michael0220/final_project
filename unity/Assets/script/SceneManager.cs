7c104c39a1bc1740a878ce645ef6f518d9b29e89
michael0220 <asdf1984620s@gmail.com>
2025-05-27 01:02:22 +0800

    新增過場動畫和其他場景

--- unity/Assets/script/SceneManager.cs
+++ unity/Assets/script/SceneManager.cs
@@ -4,10 +4,14 @@ using UnityEngine.SceneManagement;
 
 public class SceneTransitionManager : MonoBehaviour
 {
+    public CanvasGroup transitionAnim;
     public static SceneTransitionManager Instance;
     public Animator transitionAnimator;
     public float transitionTime = 1f;
-
+    void Start()
+    {
+        SetCanvasGroup(transitionAnim, false);
+    }
     void Awake()
     {
         if (Instance == null)
@@ -25,12 +29,20 @@ public class SceneTransitionManager : MonoBehaviour
     {
         StartCoroutine(LoadSceneRoutine(sceneName));
     }
-
+    void SetCanvasGroup(CanvasGroup group, bool visible)
+    {
+        group.alpha = visible ? 1 : 0;
+        group.blocksRaycasts = visible;
+        group.interactable = visible;
+    }
     IEnumerator LoadSceneRoutine(string sceneName)
     {
-        transitionAnimator.SetTrigger("Loading");
+        transitionAnimator.SetBool("Loading", true);
+        SetCanvasGroup(transitionAnim, true);
         yield return new WaitForSeconds(transitionTime);
-        transitionAnimator.ResetTrigger("Loading");
+        transitionAnimator.SetBool("Loading", false);
         SceneManager.LoadScene(sceneName);
+        yield return null;
+        SetCanvasGroup(transitionAnim, false);
     }
 }
