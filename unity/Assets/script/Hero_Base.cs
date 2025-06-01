b07f580b8901aed3a278c1050eb3144e6e19a852
michael0220 <asdf1984620s@gmail.com>
2025-06-01 17:12:02 +0800

    新增UFO

--- unity/Assets/script/Hero_Base.cs
+++ unity/Assets/script/Hero_Base.cs
@@ -7,6 +7,10 @@ public abstract class Hero_Base : MonoBehaviour
     public float currHp;
     public bool isdead = false;
 
+    private Transform attachPoint;
+    private UFOcontroller currentUFO;
+    private bool isAttach = false;
+
     public virtual void UpdateHp()
     {
         if (currHp >= maxHp) currHp = maxHp;
@@ -14,7 +18,20 @@ public abstract class Hero_Base : MonoBehaviour
         {
             currHp = 0;
             Dead();
+            if (isAttach && currentUFO != null)
+            {
+                currentUFO.DetachUFO();
+            }
         }
+    }
+
+    public virtual void AttachToUFO(Transform point, UFOcontroller UFO)
+    {
+        isAttach = true;
+        attachPoint = point;
+        currentUFO = UFO;
+        transform.position = point.position;
+        transform.SetParent(point);
     }
 
     protected abstract void Dead();
